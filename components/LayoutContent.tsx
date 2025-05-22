b2dc65b5ce7051ed8c06d2571a88d4d6977e1934
MAC mini <macmini@MACs-Mac-mini.local>
2025-05-22 14:11:50 -0700

    created layoutcontent.tsx in components

---
+++ components/LayoutContent.tsx
@@ -0,0 +1,34 @@
+// components/LayoutContent.tsx
+"use client";
+
+import { usePathname } from "next/navigation";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
+import ScrollToTop from "@/components/ScrollToTop";
+import { SessionProvider } from "next-auth/react";
+import { Providers } from "@/app/providers";
+import { AuthProvider } from "@/utils/AuthContext";
+
+export default function LayoutContent({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const isViewSection = pathname.startsWith("/view");
+
+  return (
+    <SessionProvider>
+      <AuthProvider>
+        <Providers>
+          {!isViewSection && <Header />}
+          <Sidebar />
+          <main className="w-full">{children}</main>
+          {!isViewSection && <Footer />}
+          {!isViewSection && <ScrollToTop />}
+        </Providers>
+      </AuthProvider>
+    </SessionProvider>
+  );
+}
