b01c43e77d3661a8cb0985a2f8c52f435c023d47
hk <hkinnovapath@gmail.com>
2025-07-09 21:43:03 +0530

    updated avatar code

--- components/ClientLayout.tsx
+++ components/ClientLayout.tsx
@@ -31,7 +31,53 @@
 // }
 
 
-// ClientLayout.tsx
+// // ClientLayout.tsx
+// 'use client';
+
+// import { usePathname } from "next/navigation";
+// import Header from "@/components/Header";
+// import Footer from "@/components/Footer";
+// import ScrollToTop from "@/components/ScrollToTop";
+// import Sidebar from "@/components/Sidebar";
+// import { useState, useEffect, useMemo, useCallback } from "react";
+
+// export default function ClientLayout({ children }: { children: React.ReactNode }) {
+//   const pathname = usePathname();
+//   const isViewSection = pathname.startsWith("/view");
+
+//   const [holdLoad, setHoldLoad] = useState(false);
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+//   const toggleSidebar = useCallback(() => {
+//     setSidebarOpen((prev) => !prev);
+//   }, []);
+
+//   // useEffect(() => {
+//   //   const timer = setTimeout(() => setHoldLoad(true), 300);
+//   //   return () => clearTimeout(timer);
+//   // }, []);
+
+//   const [hasMounted, setHasMounted] = useState(false);
+
+// useEffect(() => {
+//   setHasMounted(true);
+// }, []);
+
+// if (!hasMounted) return null;
+
+
+//   return holdLoad ? (
+//     <>
+//       {!isViewSection && <Header />}
+//       <Sidebar isOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
+//       <main className="w-full">{children}</main>
+//       {!isViewSection && <Footer />}
+//       {!isViewSection && <ScrollToTop />}
+//     </>
+//   ) : null;
+// }
+
+
 'use client';
 
 import { usePathname } from "next/navigation";
@@ -52,19 +98,19 @@ export default function ClientLayout({ children }: { children: React.ReactNode }
     setSidebarOpen((prev) => !prev);
   }, []);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => setHoldLoad(true), 300);
-  //   return () => clearTimeout(timer);
-  // }, []);
-
   const [hasMounted, setHasMounted] = useState(false);
 
-useEffect(() => {
-  setHasMounted(true);
-}, []);
+  // Delay rendering for 500 milliseconds
+  useEffect(() => {
+    const timer = setTimeout(() => setHoldLoad(true), 500);
+    return () => clearTimeout(timer);
+  }, []);
 
-if (!hasMounted) return null;
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
 
+  if (!hasMounted) return null;
 
   return holdLoad ? (
     <>
