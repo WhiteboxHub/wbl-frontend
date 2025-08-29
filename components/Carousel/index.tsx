ef1324518095e8c75656c44d1b894e42de9ff44e
jafarvali1 <jafarvali99@gmail.com>
2025-08-29 06:51:45 +0530

    updated the carousel animation

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -59,7 +59,7 @@ function Carousel() {
   }, [currentIndex]); // Run useEffect every time currentIndex changes
 
   return (
-    <div className="group relative mx-auto h-[180px] w-full max-w-sm px-2 sm:h-[200px] sm:max-w-md md:h-[240px] md:max-w-lg lg:h-[270px] lg:max-w-xl xl:h-[300px] xl:max-w-3xl  2xl:h-[340px] 2xl:max-w-4xl">
+    <div className="group relative mx-auto h-[180px] w-full max-w-sm px-2 sm:h-[200px] sm:max-w-md md:h-[240px] md:max-w-lg lg:h-[270px] lg:max-w-xl xl:h-[300px] xl:max-w-2xl 2xl:h-[340px] 2xl:max-w-3xl">
       <div className="relative h-full w-full rounded-2xl bg-cover bg-center">
         {slides.map((slide, index) => (
           // <Image
