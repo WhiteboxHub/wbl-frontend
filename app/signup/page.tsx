50bfe8148825d8acf78236327a56e8a394bb1f6b
Jawahar <reddynjawahar@gmail.com>
2026-02-05 07:24:00 -0800

    added work authorization as required field

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -605,7 +605,7 @@ const SignupPage = () => {
                   <div className="flex flex-col sm:flex-row gap-4 sm:gap-6">
                     <div className="mb-4 sm:mb-6 flex-1">
                       <label htmlFor="visaStatus " className="mb-2 block font-bold  text-[13px] text-dark dark:text-white">
-                        Work Authorization<span className="text-"></span>
+                        Work Authorization<span className="text-[red]">*</span>
                       </label>
                       <select
                         id="visaStatus"
