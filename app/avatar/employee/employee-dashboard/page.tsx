b7196bbdf25f346c701067326289f9762fb58790
unknown <rudraprasada0107@gmail.com>
2026-04-02 16:23:42 -0700

    Theme bug Fixed

--- app/avatar/employee/employee-dashboard/page.tsx
+++ app/avatar/employee/employee-dashboard/page.tsx
@@ -680,7 +680,7 @@ export default function EmployeeDashboardAvatarPage() {
     }
 
     return (
-        <div className="bg-gray-50 min-h-screen">
+        <div className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen">
             <EmployeeDashboardContent />
         </div>
     );
