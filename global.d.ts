d93cab576074b636f8b34a268cdb7faf83158c76
sampath-velupula <training@whitebox-learning.com>
2026-04-24 09:12:05 -0700

    Delete global.d.ts

--- global.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "react-quill/dist/quill.snow.css";
-declare module "ag-grid-community/styles/ag-grid.css";
-declare module "ag-grid-community/styles/ag-theme-alpine.css";
-declare module "@/styles/admin.css";
