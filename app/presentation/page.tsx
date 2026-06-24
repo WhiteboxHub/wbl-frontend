3ea8c935446d9d12fc6e581c1d59fd23c0034a96
raghu <s.raghugoud2001@gmail.com>
2026-06-24 06:54:50 -0700

    modified the course material in assignement to Question

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -28,7 +28,7 @@ type ComponentType =
   | "Interactive Visual Explainers"
   | "Newsletters"
   | "Books"
-  | "Assignments"
+  | "Questions"
   | "Git Repo's";
 
 export default function PresentationPage() {
@@ -56,7 +56,7 @@ export default function PresentationPage() {
     },
     { type: "Books", label: "O'Reilly Books", icon: Library },
     { type: "Newsletters", label: "Newsletters", icon: Mail },
-    { type: "Assignments", label: "Assignments", icon: ClipboardList },
+    { type: "Questions", label: "Questions", icon: ClipboardList },
     { type: "Git Repo's", label: "Git Repo's", icon: Github },
   ];
 
