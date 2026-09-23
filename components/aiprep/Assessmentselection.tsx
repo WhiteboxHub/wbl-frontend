e9e8ea5c157cf97885d5d78d960019d84d11d201
Narasimha3356 <naidunarasimha633@gmail.com>
2026-09-24 01:42:34 +0530

    Lock JD Walkthrough assessment selection

--- components/aiprep/Assessmentselection.tsx
+++ components/aiprep/Assessmentselection.tsx
@@ -38,9 +38,7 @@ const SHORT_DESCRIPTIONS: Record<AssessmentType, string> = {
 
 export const DISPLAY_CARDS: DisplayAssessmentCard[] = SUPPORTED_ASSESSMENT_TYPES.map((type) => {
   const details = ASSESSMENT_INFO_DETAILS[type];
-  const isIntro = type === 'INTRO';
-  const isJdIntro = type === 'JD_INTRO';
-  const isUnlocked = isIntro || isJdIntro;
+  const isUnlocked = type === 'INTRO';
   
   return {
     type,
