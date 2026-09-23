e8117693e0181a18c2c76840a10ad1526312b98f
Vinay Barapati <vinaybarapati2@gmail.com>
2026-09-24 04:35:19 +0530

    missing dependency on formatDate fix

--- app/avatar/candidates/credentials/page.tsx
+++ app/avatar/candidates/credentials/page.tsx
@@ -161,7 +161,7 @@ export default function CandidateCredentialsPage() {
         valueFormatter: (params) => formatDate(params.value),
       },
     ],
-    []
+    [formatDate, setSelectedResume]
   );
 
   return (
